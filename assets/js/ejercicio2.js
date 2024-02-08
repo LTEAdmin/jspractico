1a0e3750dc1ac027297ede9f54b9188f990e19c8
Alejandra Flano <aflanog@latiaencanto.cl>
2024-02-08 07:31:05 -0300

    Se actualiza ejercicio2.js, permitiendo modificar color, caja central, segun click que se realizo en color lateral

---
+++ assets/js/ejercicio2.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const btn1 = document.getElementById("btn-1"); // ACA SELECCIONAMOS EL BOTON
+const btn2 = document.getElementById("btn-2"); // ACA SELECCIONAMOS EL BOTON
+const btn3 = document.getElementById("btn-3"); // ACA SELECCIONAMOS EL BOTON
+const btn4 = document.getElementById("btn-4"); // ACA SELECCIONAMOS EL BOTON
+const btn5 = document.getElementById("btn-5"); // ACA SELECCIONAMOS EL BOTON
+const cajap = document.getElementById("caja");
+
+btn1.addEventListener("click", function () {
+  // ACA AGREGAMOS UN EVENTO AL BOTON PARA QUE CUANDO SE PRESIONE SE EJECUTE UNA ALERTA
+  /* alert(`Hola tu quieres jugar ${juegos.value} veces`); // ALERTA QUE MUESTRA EL VALOR DEL INPUT */
+       
+      cajap.style.backgroundColor = btn1.style.backgroundColor;
+    
+});
+
+btn2.addEventListener("click", function () {
+  // ACA AGREGAMOS UN EVENTO AL BOTON PARA QUE CUANDO SE PRESIONE SE EJECUTE UNA ALERTA
+  /* alert(`Hola tu quieres jugar ${juegos.value} veces`); // ALERTA QUE MUESTRA EL VALOR DEL INPUT */
+
+  cajap.style.backgroundColor = btn2.style.backgroundColor;
+});
+
+btn3.addEventListener("click", function () {
+  // ACA AGREGAMOS UN EVENTO AL BOTON PARA QUE CUANDO SE PRESIONE SE EJECUTE UNA ALERTA
+  /* alert(`Hola tu quieres jugar ${juegos.value} veces`); // ALERTA QUE MUESTRA EL VALOR DEL INPUT */
+ 
+  p.style.backgroundColor = btn3.style.backgroundColor;
+});
+
+btn4.addEventListener("click", function () {
+  // ACA AGREGAMOS UN EVENTO AL BOTON PARA QUE CUANDO SE PRESIONE SE EJECUTE UNA ALERTA
+  /* alert(`Hola tu quieres jugar ${juegos.value} veces`); // ALERTA QUE MUESTRA EL VALOR DEL INPUT */
+       
+      cajap.style.backgroundColor = btn4.style.backgroundColor;
+    
+});
+
+
+btn5.addEventListener("click", function () {
+  // ACA AGREGAMOS UN EVENTO AL BOTON PARA QUE CUANDO SE PRESIONE SE EJECUTE UNA ALERTA
+  /* alert(`Hola tu quieres jugar ${juegos.value} veces`); // ALERTA QUE MUESTRA EL VALOR DEL INPUT */
+       
+      cajap.style.backgroundColor = btn5.style.backgroundColor;
+    
+});
