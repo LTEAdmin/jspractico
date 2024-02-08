a7a2d93a156ae0e59e38e746ce6c394a3d057486
Alejandra Flano <aflanog@latiaencanto.cl>
2024-02-08 09:44:33 -0300

    me di cuenta del orden que debe tener js para que funcione correctamente el detectar el click

--- assets/js/ejercicio2.js
+++ assets/js/ejercicio2.js
@@ -1,20 +1,14 @@
 "use strict";
 
 const btn1 = document.getElementById("btn-1"); // ACA SELECCIONAMOS EL BOTON
-const btn2 = document.getElementById("btn-2"); // ACA SELECCIONAMOS EL BOTON
-const btn3 = document.getElementById("btn-3"); // ACA SELECCIONAMOS EL BOTON
-const btn4 = document.getElementById("btn-4"); // ACA SELECCIONAMOS EL BOTON
-const btn5 = document.getElementById("btn-5"); // ACA SELECCIONAMOS EL BOTON
-const cajap = document.getElementById("caja");
-
 btn1.addEventListener("click", function () {
   // ACA AGREGAMOS UN EVENTO AL BOTON PARA QUE CUANDO SE PRESIONE SE EJECUTE UNA ALERTA
   /* alert(`Hola tu quieres jugar ${juegos.value} veces`); // ALERTA QUE MUESTRA EL VALOR DEL INPUT */
-       
-      cajap.style.backgroundColor = btn1.style.backgroundColor;
-    
+
+  cajap.style.backgroundColor = btn1.style.backgroundColor;
 });
 
+const btn2 = document.getElementById("btn-2"); // ACA SELECCIONAMOS EL BOTON
 btn2.addEventListener("click", function () {
   // ACA AGREGAMOS UN EVENTO AL BOTON PARA QUE CUANDO SE PRESIONE SE EJECUTE UNA ALERTA
   /* alert(`Hola tu quieres jugar ${juegos.value} veces`); // ALERTA QUE MUESTRA EL VALOR DEL INPUT */
@@ -22,26 +16,35 @@ btn2.addEventListener("click", function () {
   cajap.style.backgroundColor = btn2.style.backgroundColor;
 });
 
+
+const btn3 = document.getElementById("btn-3"); // ACA SELECCIONAMOS EL BOTON
 btn3.addEventListener("click", function () {
   // ACA AGREGAMOS UN EVENTO AL BOTON PARA QUE CUANDO SE PRESIONE SE EJECUTE UNA ALERTA
   /* alert(`Hola tu quieres jugar ${juegos.value} veces`); // ALERTA QUE MUESTRA EL VALOR DEL INPUT */
- 
-  p.style.backgroundColor = btn3.style.backgroundColor;
+
+  cajap.style.backgroundColor = btn3.style.backgroundColor;
 });
 
+const btn4 = document.getElementById("btn-4"); // ACA SELECCIONAMOS EL BOTON
 btn4.addEventListener("click", function () {
   // ACA AGREGAMOS UN EVENTO AL BOTON PARA QUE CUANDO SE PRESIONE SE EJECUTE UNA ALERTA
   /* alert(`Hola tu quieres jugar ${juegos.value} veces`); // ALERTA QUE MUESTRA EL VALOR DEL INPUT */
-       
-      cajap.style.backgroundColor = btn4.style.backgroundColor;
-    
+
+  cajap.style.backgroundColor = btn4.style.backgroundColor;
 });
 
-
+const btn5 = document.getElementById("btn-5"); // ACA SELECCIONAMOS EL BOTON
 btn5.addEventListener("click", function () {
   // ACA AGREGAMOS UN EVENTO AL BOTON PARA QUE CUANDO SE PRESIONE SE EJECUTE UNA ALERTA
   /* alert(`Hola tu quieres jugar ${juegos.value} veces`); // ALERTA QUE MUESTRA EL VALOR DEL INPUT */
-       
-      cajap.style.backgroundColor = btn5.style.backgroundColor;
-    
+
+  cajap.style.backgroundColor = btn5.style.backgroundColor;
 });
+
+const cajap = document.getElementById("caja");
+
+
+
+
+
+
